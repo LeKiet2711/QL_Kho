f1770e9603c3e28cf7f85a691df4a92318a07524
effortivedev <loctranvan23.1.2003@gmail.com>
2024-09-24 15:59:57 +0700

    Config lại service

--- QL_Kho/Program.cs
+++ QL_Kho/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<DonViTinh_Service>();
-
+builder.Services.AddScoped<LoaiSanPham_Service>();
 
 builder.Services.AddBlazoredToast();
 
